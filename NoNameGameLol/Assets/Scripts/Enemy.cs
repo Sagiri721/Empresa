f684f03fb6ded3bddb2f09ffd66e314f0af8be3b
Rui <rfilipecs@outlook.pt>
2022-04-09 17:28:27 +0100

    added hp system

---
+++ NoNameGameLol/Assets/Scripts/Enemy.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int damage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
